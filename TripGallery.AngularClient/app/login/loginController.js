7d91ff969bb7526f80a61015ee909011f2a34549
Nazar <jasperpaladin@gmail.com>
2017-08-01 00:58:17 +0300

    Added initial skeletone for TripGallery project

---
+++ TripGallery.AngularClient/app/login/loginController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("tripGallery")
+        .controller("loginController",
+                     ["$http", LoginController]);
+
+    function LoginController($http) {
+        var vm = this;
+
+        vm.loginError = "";
+        vm.credentials = {
+            username: "",
+            password: ""
+        };
+
+        vm.submit = function ()
+        {
+            vm.loginError = "";
+
+            
+        }
+    }
+}());
